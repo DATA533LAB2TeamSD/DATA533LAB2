18e9535ae450d4b7501e7ebb662182e33e79caa9
DEBANGSHA SARKAR <debangshasarkar@dhcp-206-87-97-113.ubcvisitor.wireless.ubc.ca>
2018-11-29 14:27:36 -0800

    DATA533 Lab2

--- encryptor/encrycomplex/sub.py
+++ encryptor/encrycomplex/sub.py
@@ -1,16 +1,35 @@
 import random
-def sub(text,key=0):
-    e = ''
-    for c in text :
-        if ord(c) != 32:
-            if ord(c) == 122 :
-                e += 'a'
-            else :
-                e += chr(ord(c)+key)
-        elif ord(c) == 32:
-            e += ' '
-    return e
-#a = 'The chick is crazy'
-#e = sub(a,1)
-#print(a)
-#print(e)
+class Suben:
+    def __init__(self,text,key):
+        self.text = text
+        self.key = key
+        
+class Encr(Suben):
+    def __init__(self,text,key):
+        Suben.__init__(self,text,key)
+        self.e = ''
+        for c in text :
+            if ord(c) != 32:
+                if ord(c) == 122 :
+                    self.e += 'a'
+                else :
+                    self.e += chr(ord(c)+key)
+            elif ord(c) == 32:
+                self.e += ' '
+    def display(self):
+        return self.e
+    
+class Decr(Suben):
+    def __init__(self,text,key):
+        Suben.__init__(self,text,key)
+        self.e = ''
+        for c in text :
+            if ord(c) != 32:
+                if ord(c) == 122 :
+                    self.e += 'a'
+                else :
+                    self.e += chr(ord(c)-key)
+            elif ord(c) == 32:
+                self.e += ' '
+    def display(self):
+        return self.e
--- encryptor/encrycomplex/tran.py
+++ encryptor/encrycomplex/tran.py
@@ -5,7 +5,3 @@ def tran(a):
     for index in i:
         e += a[index]
     return e
-#a = 'The chick is crazy'
-#e = tran(a)
-#print(a)
-#print(e)
